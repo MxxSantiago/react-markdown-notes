a632c5b0fe7ffdde8d059fe6ec946a2dce94b511
Max Santiago <mxxsant@hotmail.com>
2021-11-19 19:59:10 -0800

    style: moved to components folder

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,12 +1,12 @@
 import { useCallback } from 'react';
-import useCodeMirror from './useCodemirror';
+import useCodeMirror from '../../hooks/useCodemirror';
 
 const Editor = ({ onChange, initialDoc }) => {
     const handleChange = useCallback(
         (state) => onChange(state.doc.toString()),
         [onChange]
     );
-    const [refContainer, editorView] = useCodeMirror({
+    const [refContainer] = useCodeMirror({
         initialDoc: initialDoc,
         onChange: handleChange,
     });
