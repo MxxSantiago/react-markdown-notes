d2f88a815f98152805a4d8e9b612b5c0e3ea6a77
Max Santiago <mxxsant@hotmail.com>
2021-11-24 15:15:25 -0800

    feat: incorpored SCSS module

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,17 +1,20 @@
 import { useCallback } from 'react';
 import useCodeMirror from '../../hooks/useCodemirror';
 
+import { editor } from './editor.module.scss';
+
 const Editor = ({ onChange, initialDoc }) => {
     const handleChange = useCallback(
         (state) => onChange(state.doc.toString()),
         [onChange]
     );
+
     const [refContainer] = useCodeMirror({
         initialDoc: initialDoc,
         onChange: handleChange,
     });
 
-    return <div className="editor" ref={refContainer}></div>;
+    return <div className={editor} ref={refContainer}></div>;
 };
 
 export default Editor;
