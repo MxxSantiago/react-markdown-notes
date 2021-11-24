872d20e306055b1246beae1b3adbc8f765d55f7e
Max Santiago <mxxsant@hotmail.com>
2021-11-24 15:14:29 -0800

    feat: incorpored ResizePanel

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,15 +1,34 @@
 import ReactMarkdown from 'react-markdown';
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
+import ResizePanel from 'react-resize-panel';
+
+import { useIsSmallDevice } from '../../hooks/useIsSmallDevice';
+
 import { components } from '../../react-markdown-components';
 
+import { preview } from './preview.module.scss';
+
 const Preview = ({ document }) => {
+    const isSmallDevice = useIsSmallDevice();
+
     return (
-        <div className="preview">
-            <ReactMarkdown
-                children={document}
-                components={ChakraUIRenderer(components)}
-            />
-        </div>
+        <>
+            {!isSmallDevice ? (
+                <ResizePanel direction="w" handleClass="0-0-0">
+                    <ReactMarkdown
+                        className={preview}
+                        children={document}
+                        components={ChakraUIRenderer(components)}
+                    />
+                </ResizePanel>
+            ) : (
+                <ReactMarkdown
+                    className={preview}
+                    children={document}
+                    components={ChakraUIRenderer(components)}
+                />
+            )}
+        </>
     );
 };
 
