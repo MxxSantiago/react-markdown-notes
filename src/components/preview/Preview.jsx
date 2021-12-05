e95cabbebc5602ea001d24c6604865a0a059fd50
Max Santiago <mxxsant@hotmail.com>
2021-12-04 19:53:20 -0800

    feat: removing the configuration that was necessary to use the react-resize-panel library, and adding a few minimal changes

--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,34 +1,17 @@
 import ReactMarkdown from 'react-markdown';
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
-import ResizePanel from 'react-resize-panel';
-
-import { useIsSmallDevice } from '../../hooks/useIsSmallDevice';
 
 import { components } from '../../react-markdown-components';
 
 import { preview } from './preview.module.scss';
 
 const Preview = ({ document }) => {
-    const isSmallDevice = useIsSmallDevice();
-
     return (
-        <>
-            {!isSmallDevice ? (
-                <ResizePanel direction="w" handleClass="0-0-0">
-                    <ReactMarkdown
-                        className={preview}
-                        children={document}
-                        components={ChakraUIRenderer(components)}
-                    />
-                </ResizePanel>
-            ) : (
-                <ReactMarkdown
-                    className={preview}
-                    children={document}
-                    components={ChakraUIRenderer(components)}
-                />
-            )}
-        </>
+        <ReactMarkdown
+            className={preview}
+            children={document}
+            components={ChakraUIRenderer(components)}
+        />
     );
 };
 
