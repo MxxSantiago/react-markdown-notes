2e078e504d128aad8ab5701c2e68aec5dc338d51
Max Santiago <mxxsant@hotmail.com>
2021-12-06 01:34:21 -0800

    feat: adding a function on small devices to be able to open the sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,14 +2,37 @@ import { useIsSmallDevice } from '../../hooks/useIsSmallDevice';
 
 import Sidebar from '../sidebar/Sidebar.component';
 
+import { useBoolean } from '@chakra-ui/hooks';
+
+import { Box } from '@chakra-ui/layout';
+import { IconButton } from '@chakra-ui/button';
+
+import { CgMenu } from 'react-icons/cg';
+
 import { navbar } from './navbar.module.scss';
 
 const Navbar = () => {
+    const [flag, setFlag] = useBoolean();
     const isSmallDevice = useIsSmallDevice();
 
     return (
         <nav className={navbar}>
-            {!isSmallDevice ? <Sidebar /> : <p>header</p>}
+            {!isSmallDevice ? (
+                <Sidebar />
+            ) : (
+                <Box w="100vw">
+                    <Box bg="gray.700" w="100%" h="100%">
+                        <IconButton
+                            bg="gray.700"
+                            borderRadius="0"
+                            icon={<CgMenu />}
+                            _focus={'box-shadow: none;'}
+                            onClick={setFlag.toggle}
+                        />
+                    </Box>
+                    {flag ? <Sidebar /> : null}
+                </Box>
+            )}
         </nav>
     );
 };
