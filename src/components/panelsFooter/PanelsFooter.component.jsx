67e809d551dae9cdac39ff84e9732746dde452d6
Max Santiago <mxxsant@hotmail.com>
2021-12-06 01:46:41 -0800

    feat: Adding buttons tooltips and deleting unnecessary code

--- src/components/panelsFooter/PanelsFooter.component.jsx
+++ src/components/panelsFooter/PanelsFooter.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { useBoolean } from '@chakra-ui/hooks';
 import { IconButton } from '@chakra-ui/button';
 import { Box } from '@chakra-ui/layout';
 
@@ -12,9 +11,8 @@ import {
 import { CgScan, CgCopy, CgTrash, CgSoftwareDownload } from 'react-icons/cg';
 
 import { panelsFooter, panelsFooterIcon } from './panels-footer.module.scss';
-import { useEffect } from 'react';
 
-const PanelsFooter = ({ view, setView, IViews }) => {
+const PanelsFooter = ({ setView, IViews }) => {
     const { ON, OFF, SPLITTED } = IViews;
 
     return (
@@ -24,16 +22,19 @@ const PanelsFooter = ({ view, setView, IViews }) => {
                     bg="gray.700"
                     className={panelsFooterIcon}
                     icon={<CgTrash />}
+                    title="Delete note"
                 />
                 <IconButton
                     bg="gray.700"
                     className={panelsFooterIcon}
                     icon={<CgCopy />}
+                    title="Copy content to clipboard"
                 />
                 <IconButton
                     bg="gray.700"
                     className={panelsFooterIcon}
                     icon={<CgSoftwareDownload />}
+                    title="Download .md file"
                 />
             </div>
             <div>
@@ -42,23 +43,27 @@ const PanelsFooter = ({ view, setView, IViews }) => {
                     className={panelsFooterIcon}
                     icon={<CgScan />}
                     onClick={() => setView(SPLITTED)}
+                    title="Toggle views"
                 />
                 <IconButton
                     bg="gray.700"
                     className={panelsFooterIcon}
                     icon={<IoEyeOffOutline />}
                     onClick={() => setView(OFF)}
+                    title="Hide preview"
                 />
                 <IconButton
                     bg="gray.700"
                     className={panelsFooterIcon}
                     icon={<IoEyeOutline />}
                     onClick={() => setView(ON)}
+                    title="Hide editor"
                 />
                 <IconButton
                     bg="gray.700"
                     className={panelsFooterIcon}
                     icon={<IoSettingsOutline />}
+                    title="Settings"
                 />
             </div>
         </Box>
