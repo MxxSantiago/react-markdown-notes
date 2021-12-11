f61d1b26dc90285918a199ed153bbb97dfdc919f
Max Santiago <mxxsant@hotmail.com>
2021-12-10 16:55:37 -0800

    fix: added keys to Note components

--- src/components/Notes/Notes.component.jsx
+++ src/components/Notes/Notes.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import Note from '../navigationItem/note.component';
+import Note from '../navigationItem/Note.component';
 
 import { notes } from './notes.module.scss';
 
 const Notes = ({ items }) => {
     return (
         <ul className={notes}>
-            {items.map((item) => (
-                <Note data={item} />
+            {items.map((item, index) => (
+                <Note data={item} key={index} />
             ))}
         </ul>
     );
