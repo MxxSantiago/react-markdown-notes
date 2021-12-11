5b24576458dad4b7a0c334f9020080fe0deb0ce9
Max Santiago <mxxsant@hotmail.com>
2021-12-10 16:49:15 -0800

    feat: implementing the useNotes hook

--- src/components/panelsFooter/PanelsFooter.component.jsx
+++ src/components/panelsFooter/PanelsFooter.component.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
 
+import useNotes from '../../hooks/useNotes';
+
 import { IconButton } from '@chakra-ui/button';
 import { Box } from '@chakra-ui/layout';
+import { Tooltip } from '@chakra-ui/react';
 
 import {
     IoEyeOutline,
     IoEyeOffOutline,
     IoSettingsOutline,
+    IoSaveOutline,
 } from 'react-icons/io5';
 import { CgScan, CgCopy, CgTrash, CgSoftwareDownload } from 'react-icons/cg';
 
 import { panelsFooter, panelsFooterIcon } from './panels-footer.module.scss';
-import { Tooltip } from '@chakra-ui/react';
 
-const PanelsFooter = ({ setView, IViews }) => {
+const PanelsFooter = ({ setView, IViews, doc }) => {
+    let { notes, activeNote, deleteNote } = useNotes();
+
     const { ON, OFF, SPLITTED } = IViews;
+
+    const saveNoteContent = () => {
+        activeNote.content = doc.toString();
+    };
 
     return (
         <Box bg="gray.700" className={panelsFooter}>
             <div>
-                <Tooltip label="Delete note">
-                    <IconButton
-                        bg="gray.700"
-                        className={panelsFooterIcon}
-                        icon={<CgTrash />}
-                    />
-                </Tooltip>
+                {notes.notes.length > 1 ? (
+                    <Tooltip label="Delete note">
+                        <IconButton
+                            bg="gray.700"
+                            className={panelsFooterIcon}
+                            icon={<CgTrash />}
+                            onClick={() => deleteNote(notes.activeNote)}
+                        />
+                    </Tooltip>
+                ) : null}
                 <Tooltip label="Copy content to clipboard">
                     <IconButton
                         bg="gray.700"
@@ -38,6 +50,16 @@ const PanelsFooter = ({ setView, IViews }) => {
                         bg="gray.700"
                         className={panelsFooterIcon}
                         icon={<CgSoftwareDownload />}
+                    />
+                </Tooltip>
+                <Tooltip label="Save changes">
+                    <IconButton
+                        colorScheme="green"
+                        variant="outline"
+                        border="none"
+                        className={panelsFooterIcon}
+                        icon={<IoSaveOutline />}
+                        onClick={saveNoteContent}
                     />
                 </Tooltip>
             </div>
