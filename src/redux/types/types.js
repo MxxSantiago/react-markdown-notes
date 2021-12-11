9a2045455047998f585394d49ef793c4e52fd110
Max Santiago <mxxsant@hotmail.com>
2021-12-10 16:33:52 -0800

    feat: added redux types

---
+++ src/redux/types/types.js
@@ -0,0 +1,9 @@
+const types = {
+    // notes
+    notesActiveNote: '[Notes] : Setted active note',
+    notesAddNewNote: '[Notes] : Added new note',
+    notesDeleteNote: '[Notes] : Deleted note',
+    notesUpdatedNote: '[Notes] : Updated note',
+};
+
+export default types;
