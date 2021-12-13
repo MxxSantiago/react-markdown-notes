47f5122a2761c1143bc2d3ef1dde2ee35bdefdbb
Max Santiago <mxxsant@hotmail.com>
2021-12-13 10:53:40 -0800

    feat: added the function of being able to search notes

--- src/components/searchTask/SearchTask.component.jsx
+++ src/components/searchTask/SearchTask.component.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from 'react';
+
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input';
 
 import { IoSearchOutline } from 'react-icons/io5';
+import useNotes from '../../hooks/useNotes';
 
-const SearchTask = () => {
+const SearchTask = ({ setVisibleNotes }) => {
+    const { notes } = useNotes();
+    const [search, setSearch] = useState('');
+
+    const handleChange = ({ target }) => {
+        setSearch(target.value);
+    };
+
+    useEffect(() => {
+        const results = notes.notes.filter((note) =>
+            note.title.toLowerCase().includes(search.toLowerCase())
+        );
+
+        setVisibleNotes(results);
+    }, [search]);
+
     return (
         <InputGroup>
             <InputLeftElement children={<IoSearchOutline color="#5B5F5E" />} />
             <Input
                 variant="filled"
+                value={search}
                 placeholder="Search"
                 borderRightRadius="0"
+                onChange={handleChange}
             />
         </InputGroup>
     );
