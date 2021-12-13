d289310b5df775a3aa88e1d0d72c72b53b3fbaf8
Max Santiago <mxxsant@hotmail.com>
2021-12-13 10:43:24 -0800

    feat: behavior of saveChangesButton component has been improved.

--- src/components/saveChangesButton/SaveChangesButton.component.jsx
+++ src/components/saveChangesButton/SaveChangesButton.component.jsx
@@ -1,19 +1,37 @@
 import useNotes from '../../hooks/useNotes';
 
-import { IconButton, Tooltip } from '@chakra-ui/react';
+import { IconButton, Tooltip, useBoolean, useToast } from '@chakra-ui/react';
 
 import { IoSaveOutline } from 'react-icons/io5';
 
 const SaveChangesButton = ({ document }) => {
     let { activeNote } = useNotes();
+    const toast = useToast();
+
+    const [flag, setFlag] = useBoolean();
 
     const saveNoteContent = () => {
         activeNote.content = document.toString();
+
+        toast({
+            title: 'Changes saved!!',
+            status: 'success',
+            duration: 1250,
+            isClosable: true,
+            position: 'top-right',
+        });
+
+        setFlag.toggle();
+
+        setTimeout(() => {
+            setFlag.toggle();
+        }, 1500);
     };
 
     return (
         <Tooltip label="Save changes">
             <IconButton
+                disabled={flag}
                 border="none"
                 borderRadius="0"
                 colorScheme="green"
