5b8c7df9ee12c6b10c977cf912f42266b0a49e29
Max Santiago <mxxsant@hotmail.com>
2021-12-13 17:06:28 -0800

    feat: created AboutModal component

---
+++ src/components/aboutModal/AboutModal.component.jsx
@@ -0,0 +1,72 @@
+import {
+    Divider,
+    Heading,
+    Link,
+    ListItem,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    UnorderedList,
+} from '@chakra-ui/react';
+
+import { CgExternal } from 'react-icons/cg';
+
+const AboutModal = ({ isOpen, onClose }) => {
+    return (
+        <Modal
+            size="lg"
+            isOpen={isOpen}
+            onClose={onClose}
+            isCentered={true}
+            motionPreset="slideInBottom"
+        >
+            <ModalOverlay />
+            <ModalContent mx="5">
+                <ModalHeader bg="gray.600">About</ModalHeader>
+                <Divider />
+                <ModalCloseButton />
+                <ModalBody pb={6}>
+                    <Heading as="h4" size="md" mt="3" mb="2">
+                        Markdown resources
+                    </Heading>
+                    <UnorderedList>
+                        <ListItem>
+                            <Link
+                                href="https://www.markdownguide.org/basic-syntax/"
+                                isExternal
+                                color="blue.200"
+                            >
+                                Learn the basic Syntax
+                            </Link>
+                        </ListItem>
+                        <ListItem>
+                            <Link
+                                href="https://www.markdownguide.org/extended-syntax/"
+                                isExternal
+                                color="blue.200"
+                            >
+                                Learn the extended Syntax
+                            </Link>
+                        </ListItem>
+                    </UnorderedList>
+                    <Divider my="3" />
+                    <Link
+                        d="flex"
+                        href="https://github.com/MxxSantiago/react-markdown-notes"
+                        isExternal
+                        alignItems="center"
+                    >
+                        Project source code{' '}
+                        <CgExternal style={{ marginTop: '3px' }} />
+                    </Link>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    );
+};
+
+export default AboutModal;
