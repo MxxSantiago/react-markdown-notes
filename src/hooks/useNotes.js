63e33837c0823cf6b78834416b444872a8b0d992
Max Santiago <mxxsant@hotmail.com>
2021-12-13 17:15:34 -0800

    feat: incorpored saveNoteContent action to useNotes hook

--- src/hooks/useNotes.js
+++ src/hooks/useNotes.js
@@ -4,12 +4,18 @@ import {
     changeActiveNote,
     addNewNote,
     deleteNote,
+    saveNoteContent,
 } from '../redux/actions/notes';
 
 const useNotes = () => {
     const notes = useSelector((state) => state.notes);
 
-    const actions = useActions({ changeActiveNote, addNewNote, deleteNote });
+    const actions = useActions({
+        changeActiveNote,
+        addNewNote,
+        deleteNote,
+        saveNoteContent,
+    });
 
     const activeNote = notes.notes.find((note) => note.id === notes.activeNote);
 
