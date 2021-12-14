8f52c49e08cd9ab0a6105592049c55a3ba0b6edc
Max Santiago <mxxsant@hotmail.com>
2021-12-13 17:11:40 -0800

    feat: added initial note which will be loaded when using the application for the first time

---
+++ src/initial-note.js
@@ -0,0 +1,24 @@
+const initialNoteContent = `## Welcome to *Markdown* Notes
+
+![ ](https://media.giphy.com/media/eNAsjO55tPbgaor7ma/giphy.gif)
+
+Here you can create and manage your notes created with **Markdown**, a special application for those who like to write markdown, it is also quite useful to reinforce their knowledge in this language ✍
+
+This app uses **GitHub Flavored Markdown**, the dialect of Markdown that is currently supported for user content on and GitHub 😎
+
+---
+
+Learn [the basic markdown syntax](https://www.markdownguide.org/basic-syntax/).
+`;
+
+export const initialNote = {
+    content: initialNoteContent,
+    id: 928734,
+    tags: [
+        {
+            title: 'Example',
+            color: 'pink',
+        },
+    ],
+    title: 'Welcome to Markdown Notes',
+};
